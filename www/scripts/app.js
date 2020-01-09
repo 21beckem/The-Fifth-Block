a49e6c7fe8e722a0ec8b28a84e01bcbf1c2a4a72
21beckem <21beckem@users.noreply.github.com>
2020-01-08 19:29:59 -0600

    trying again

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -1,9 +1,7 @@
 var input = document.getElementById('messageInput');
 var that = this;
-while (document.getElementById("sendTypingData").checked == true) {
-    if (input.value != "") {
-        that.socket.emit('starttyping');
-        while(input.value != "") {}
-        that.socket.emit('stoptyping');
-    }
+if (input.value != "") {
+    that.socket.emit('starttyping');
+    while(input.value != "") {}
+    that.socket.emit('stoptyping');
 }
